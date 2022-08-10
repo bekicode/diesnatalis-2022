4c86364e0097583e7872d0fe74c9b52ab94c3390
Mochammad Hanif <mochanifz@gmail.com>
2022-08-10 17:14:24 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,14 +16,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('/');
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::middleware([
+        'user-access:user'
+    ])->group( function()
+    {
+        Route::get('/competition', [UserController::class, 'index'])->name('competition.index');
+        Route::get('/competition/create', [UserController::class, 'create'])->name('competition.create');
+        Route::POST('/competition/create', [UserController::class, 'post'])->name('competition.post');
+        Route::get('/competition/{id}', [UserController::class, 'detail'])->name('competition.detail');
+        Route::POST('/competition/{id}', [UserController::class, 'snap'])->name('competition.snap');
+        Route::get('/check/{id}', [UserController::class, 'check'])->name('competition.check');
+    });
 });
