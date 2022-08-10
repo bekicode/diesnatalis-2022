8db751cbf8cf402707a05c25f7ce901af22663eb
Mochammad Hanif <mochanifz@gmail.com>
2022-08-10 17:12:48 +0700

    Create UserAccess.php

---
+++ app/Http/Middleware/UserAccess.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Auth;
+
+class UserAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next, $userRole)
+    {
+        if($userRole == 'admin')
+        {
+            $userRole = 1;
+        }
+        else
+        {
+            $userRole = 0;
+        }
+        if(Auth::user()->role == $userRole)
+        {
+            return $next($request);
+        }
+        return abort(404);
+    }
+}
