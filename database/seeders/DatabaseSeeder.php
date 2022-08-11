b237f76d16a939c52b723f7803e1f0acc163f1b0
Mochammad Hanif <mochanifz@gmail.com>
2022-08-11 23:29:49 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,29 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+<<<<<<< Updated upstream
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+=======
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@sevent.id',
+            'password' => Hash::make("admin123"),
+            'phone' => '0',
+            'role' => '2',
+        ]);
+
+        \App\Models\Competition::create([
+            'name' => 'UI/UX Designer Competition',
+            'price' => '45000',
+        ]);
+
+        \App\Models\Competition::create([
+            'name' => 'Web Developer Competition',
+            'price' => '45000',
+        ]);
+>>>>>>> Stashed changes
     }
 }
