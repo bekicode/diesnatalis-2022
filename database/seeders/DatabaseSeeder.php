9b63ad32bfb38c485a59a4b79b99de2a52ea7465
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2022-08-11 12:44:38 +0700

    feat: add seeder for User role admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,10 +17,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@sevent.id',
+            'password' => Hash::make("admin123"),
+            'phone' => '0',
+            'role' => '2',
+        ]);
 
         \App\Models\Competition::create([
             'name' => 'UI/UX Designer Competition',
