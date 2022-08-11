20ba303ef87da51d68a09f982cb7791ae64a735e
Mochammad Hanif <mochanifz@gmail.com>
2022-08-11 23:30:17 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
+<<<<<<< Updated upstream
     Route::middleware([
         'user-access:user'
     ])->group( function()
@@ -34,4 +35,42 @@ Route::middleware([
         Route::POST('/competition/{id}', [UserController::class, 'snap'])->name('competition.snap');
         Route::get('/check/{id}', [UserController::class, 'check'])->name('competition.check');
     });
+=======
+
+    // user
+    Route::middleware(['user-access:user'])
+        ->controller(UserCompititionController::class)
+        ->group( function()
+        {
+            Route::get('/competition', 'index')->name('competition.index');
+            Route::get('/competition/create', 'create')->name('competition.create');
+            Route::POST('/competition/create', 'post')->name('competition.post');
+            Route::get('/competition/{id}', 'detail')->name('competition.detail');
+            Route::POST('/competition/{id}', 'snap')->name('competition.snap');
+            Route::get('/check/{id}', 'check')->name('competition.check');
+            Route::get('/competition/{id}/add', 'add')->name('competition.add');
+            Route::POST('/competition/{id}/add', 'participantPost')->name('competition.participant-post');
+            Route::get('/competition/{id_teams}/{id}', 'participantEdit')->name('competition.participant-edit');
+            Route::POST('/competition/{id_teams}/{id}', 'participantEditPost')->name('competition.participant-edit-post');
+        });
+        
+    // admin
+    Route::prefix('admin')
+        ->middleware(['user-access:admin'])
+        ->controller(AdminController::class)
+        ->name('admin.')
+        ->group( function()
+        {
+            // competition
+            Route::get('/competition', 'list_competition')->name('list_competition');
+            Route::get('/competition/tambah', 'tambah_competition')->name('tambah_competition');
+            Route::post('/competition/tambah', 'tambah_competition_act')->name('tambah_competition_act');
+            Route::get('/competition/update/{id}', 'update_competition')->name('update_competition');
+            Route::post('/competition/update/{id}', 'update_competition_act')->name('update_competition_act');
+
+            // competition
+            Route::get('/team/uiux', 'list_team_ui_ux')->name('list_team_ui_ux');
+            Route::get('/team/web', 'list_team_web')->name('list_team_web');
+        });
+>>>>>>> Stashed changes
 });
