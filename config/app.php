162739d52f7aed070ecb305e4ec0ab78e783ffe3
Mochammad Hanif <mochanifz@gmail.com>
2022-08-13 12:39:36 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -196,6 +196,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
+        HTMLMin\HTMLMin\HTMLMinServiceProvider::class,
 
     ],
 
@@ -212,6 +213,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'HTMLMin' => HTMLMin\HTMLMin\Facades\HTMLMin::class,
     ])->toArray(),
 
 ];
