d2abd1e21296d39952eca9515d2afbf3edab241b
Mochammad Hanif <mochanifz@gmail.com>
2022-08-14 21:12:58 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,9 +11,9 @@
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        <link rel="stylesheet" href="{{asset('build/assets/app.b21c011a.css')}}">
-        <script src="{{asset('build/assets/app.d225c007.js')}}"></script>
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        {{-- <link rel="stylesheet" href="{{asset('build/assets/app.b21c011a.css')}}">
+        <script src="{{asset('build/assets/app.d225c007.js')}}"></script> --}}
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Styles -->
         @livewireStyles
